3a8606ee4c9d3cf22243b380dab7e9155f17dbe2
SamHuang7880 <51182244+SamHuang7880@users.noreply.github.com>
2020-06-22 11:22:05 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for boss project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'boss'
+
+SPIDER_MODULES = ['boss.spiders']
+NEWSPIDER_MODULE = 'boss.spiders'
+
+
+# 渲染服务的url
+SPLASH_URL = 'http://192.168.99.100:8050/'
+
+# 去重过滤器
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1'
+
+USER_AGENT_LIST=[
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+
+PROXIES = ['58.208.116.83:8060', '116.226.59.25:8060', '124.94.246.24:8060', '125.107.126.139:8060', '180.175.2.164:8060', '122.234.171.17:8060', '39.137.69.10:8080', '39.137.69.7:8080', '39.137.69.9:8080', '120.76.77.152:9999', '118.187.58.34:53281', '183.237.206.92:53281', '221.2.174.28:8060', '115.159.31.195:8080', '60.217.156.158:8060', '116.226.159.211:8060', '118.250.2.8:8060', '114.252.171.174:8060', '119.180.168.199:8060', '114.243.73.185:8060', '118.190.94.254:9001', '223.203.0.14:8000', '120.24.156.230:3128', '101.132.122.230:3128', '117.114.149.10:45801', '106.14.197.219:8118', '39.137.20.87:8080', '210.77.24.191:1080', '116.192.171.51:51057', '58.247.127.145:53281', '125.37.172.202:8118', '125.39.9.34:9000', '125.39.9.35:9000', '111.165.100.170:8060', '221.239.86.26:32228', '117.131.235.198:8060', '61.128.208.94:3128', '222.182.121.31:8118', '27.191.234.69:9999', '124.237.83.14:53281', '183.196.97.125:41397', '221.205.89.161:9797', '118.81.69.56:9797', '118.81.69.8:9797', '183.203.214.148:8118', '59.48.237.6:8060', '42.59.84.83:1133', '61.176.223.7:58822', '59.44.247.194:9797', '113.235.181.163:9999', '218.60.8.98:3129', '175.148.100.217:1133', '113.235.181.163:9797', '175.17.154.157:8080', '222.162.57.92:63000', '1.188.144.85:80', '111.40.84.73:9999', '221.206.100.133:54781', '111.40.84.73:9797', '111.43.70.58:51547', '221.6.201.18:9999', '112.25.129.174:40079', '202.119.248.8:80', '115.206.129.166:9797', '121.42.242.77:8000', '218.71.161.124:80', '112.12.37.196:53281', '112.14.158.86:40381', '58.87.98.150:1080', '58.87.68.189:1080', '183.245.99.52:80', '183.131.154.143:10328']
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 1
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+COOKIES_ENABLED = True
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+#Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.5,en;q=0.3",
+    "Accept-Encoding": "gzip, deflate",
+    'Content-Length': '0',
+    "Connection": "keep-alive"
+}
+
+#RANDOM_UA_TYPE= 'random'
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+SPIDER_MIDDLEWARES = {
+   'boss.middlewares.BossSpiderMiddleware': 543,
+   'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+    'boss.middlewares.BossDownloaderMiddleware': 1,
+    #'boss.middlewares.RandomUserAgentMiddleware': 543,
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+MYSQL_DB_NAME = 'mysql'
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+#CHARSET = 'utf8mb4'
+
+
+ITEM_PIPELINES = {
+   #'boss.pipelines.QQVideoCommentPipeline': 1,
+   'boss.pipelines.BossPipeline': 401,
+   #'boss.pipelines.ProxyPipeline': 1,
+   #'boss.pipelines.BlogStar2018Pipeline': 1,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# 使用Splash的Http缓存
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
+#允许出现的错误码
+HTTPERROR_ALLOWED_CODES = [521]
